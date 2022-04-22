7d866f37de732e98e2e872e762bc4e52cf3cbabb
ana100431219 <99047536+ana100431219@users.noreply.github.com>
2022-04-22 18:32:51 +0200

    Update group_6_mvp.py

--- group_6_mvp.py
+++ group_6_mvp.py
@@ -44,39 +44,3 @@ df_best=df_selcountry.groupby(['shortName','name','activityType','organizationUR
 df_best.to_excel("country_participants.xlsx")
 df_best.head()
 
-from sqlite3.dbapi2 import DatabaseError
-import sqlite3
-import pandas as pd
-drive.mount('/content/drive', force_remount=True)
-
-df_participants=pd.read_excel('/content/drive/MyDrive/participants.xlsx')
-df_countries=pd.read_excel('/content/drive/MyDrive/countries.xlsx')
-df_projects=pd.read_excel('/content/drive/MyDrive/projects.xlsx')
-
-con= sqlite3.connect('exel_database.db')
-df_projects.to_sql('projects',con, if_exists='replace', index= False)
-df_countries.to_sql('countries',con, if_exists='replace', index= False)
-df_participants.to_sql('participants',con, if_exists='replace', index= False)
-
-con.close()
-df_projects.head()
-
-database = exel_database.db
-selects= {
-'country':
-'''SELECT Acronym FROM countries WHERE Country = "{}" ''',
-
-'grants':
-'''SELECT SUM (O.ecContribution) AS grants
-  FROM organizations o JOIN projects p ON o.projectID==p.projectID
-  WHERE o.country = '{}'
-  GROUP BY p.year''',
-
-'participants':
-'''SELECT shortName, o.name, p.acronym, p.keywords
-  FROM organizations
-  WHERE country = '{}'
-  GROUP BY name ORDER BY SUM(ecContribution) DESC''',
-
-'coordinators':
-'''SELECT o.shortName
