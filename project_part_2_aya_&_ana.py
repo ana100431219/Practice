2a6807c44bb5a9a6ec20480ab62d0eecffa7e54b
ana100431219 <99047536+ana100431219@users.noreply.github.com>
2022-05-06 20:46:08 +0200

    Update project_part_2_aya_&_ana.py

--- project_part_2_aya_&_ana.py
+++ project_part_2_aya_&_ana.py
@@ -5,7 +5,7 @@ from PIL import Image
 
 
 #Select country
-conn=sqlite3.connect(database)
+con=sqlite3.connect(database)
 ct= st.selectbox('Select country', ['Spain', 'France', 'Germany'])
 country=pd.read_sql(selects['country'].format(ct), conn)
 country=country.Acronym.item()
