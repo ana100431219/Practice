1dd67730be07d3532a9c15d153e0a4ae237a83cd
ana100431219 <99047536+ana100431219@users.noreply.github.com>
2022-05-06 20:44:55 +0200

    Update project_part_2_aya_&_ana.py

--- project_part_2_aya_&_ana.py
+++ project_part_2_aya_&_ana.py
@@ -4,17 +4,17 @@ from sqlite3.dbapi2 import DatabaseError
 from PIL import Image
 
 
-#Title
-image=Image.open('KDT-JU.png')
-st.image(image)
-st.title('Partner search tool')
-
 #Select country
 conn=sqlite3.connect(database)
 ct= st.selectbox('Select country', ['Spain', 'France', 'Germany'])
 country=pd.read_sql(selects['country'].format(ct), conn)
 country=country.Acronym.item()
 st.write(f'You selected: {country}-{ct}')
+
+#Title
+image=Image.open('KDT-JU.png')
+st.image(image)
+st.title('Partner search tool')
 
 #Other selects
 dfs={}
