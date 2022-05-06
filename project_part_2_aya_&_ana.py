24d0209f34f1491151a8391eae8974fe137baceb
ana100431219 <99047536+ana100431219@users.noreply.github.com>
2022-05-06 20:46:45 +0200

    Update project_part_2_aya_&_ana.py

--- project_part_2_aya_&_ana.py
+++ project_part_2_aya_&_ana.py
@@ -15,33 +15,3 @@ st.write(f'You selected: {country}-{ct}')
 image=Image.open('KDT-JU.png')
 st.image(image)
 st.title('Partner search tool')
-
-#Other selects
-dfs={}
-for key,sel in selects.items():
-  dfs[key]=pd.read_sql(sel.format(country), conn)
-
-df_grants_year = pd.read_sql('''SELECT p.year, SUM(o.ecContribution) AS grants
-    FROM organizations o JOIN projects p ON o.projectID==p.projectID
-    WHERE o.country='{}'
-    GROUP BY p.year '''.format(country), conn)
-
-#grants
-st.subheader(f'Yearly EC contribution in {ct} (€)')
-st.bar_chart(dfs['grants'])
-
-#participants
-st.subheader(f'Participants in {ct}')
-st.dataframe(dfs['participants'])
-csv_p=dfs['participants'].to_csv().encode('utf-8')
-st.download_button(
-    label=''Download participants data as CSV",
-    data=csv_p,
-    file_name=f'{country}_participants.csv',
-    mime='text/csv',
-)
-
-#coordinators
-st.subheader(f'Project coordinators in {ct}')
-st.dataframe(dfs['coordinators'])
-csv_c=dfs['coordinators'].to_csv().encode('utf-8')
