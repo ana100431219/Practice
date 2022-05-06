c3763c089fcc02fe64523e0f0b8c63ff229519bb
ana100431219 <99047536+ana100431219@users.noreply.github.com>
2022-05-06 20:49:04 +0200

    Update project_part_2_aya_&_ana.py

--- project_part_2_aya_&_ana.py
+++ project_part_2_aya_&_ana.py
@@ -2,9 +2,8 @@ import sqlite3
 import pandas as pd 
 from PIL import Image
 
-
 #Select country
-con=sqlite3.connect(ecsel_database.db)
+conn=sqlite3.connect(ecsel_database.db)
 ct= st.selectbox('Select country', ['Spain', 'France', 'Germany'])
 country=pd.read_sql(selects['country'].format(ct), conn)
 country=country.Acronym.item()
