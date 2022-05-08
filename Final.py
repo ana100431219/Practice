e9f47e1171a173937132967701fe520caf9983f8
ana100431219 <99047536+ana100431219@users.noreply.github.com>
2022-05-08 22:11:47 +0200

    Update Final.py

--- Final.py
+++ Final.py
@@ -25,7 +25,7 @@ st.title('Partner search tool')
 #Select country
 conn=sqlite3.connect(database)
 ct= st.selectbox('Select country', ['Spain', 'France', 'Germany'])
-country=pd.read_sql('''SELECT[Country]'''.format(ct), conn)
+country=pd.read_sql(SELECT[Country].format(ct), conn)
 country=country.Acronym.item()
 st.write(f'You selected: {country}-{ct}')
 
